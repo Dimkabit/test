07c64461982845feb506f2440eeb2e59dcd668df
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2022-01-29 16:53:37 +0200

    Add files via upload

--- js/sliders.js
+++ js/sliders.js
@@ -55,15 +55,18 @@ function initSliders() {
 			
 			breakpoints: {
 				320: {
+					slidesPerView: 1,
+					spaceBetween: 100,
+					
+				},
+				454: {
 					slidesPerView: 2,
-					spaceBetween: 30,
-					autoHeight: true,
+					spaceBetween: 20,
 				},
 				768: {
 					slidesPerView: 3,
 					spaceBetween: 20,
-				},
-				
+				}
 			},
 			
 			on: {
